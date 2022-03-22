42caf9a7e22cf8fdac83918fc98e3617fc869996
Ivan Volzhenin <rorbekgraf@gmail.com>
2022-03-22 19:46:08 +0300

    2 commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports ={
+    env:{
+        browser: true,
+        node:true,
+        e2020:true
+    },
+    extends:['airbnb-base'],
+    parserOption:{
+        sourceType:'moule',
+        ecmaVersion: 11,
+
+
+    },
+    rules:{
+        'no-console':0,
+    }
+}
